45c393b08c38cd4118a745363d9d1376a71e9fbb
Edor Kacerja <edorkacerja@gmail.com>
2015-11-29 03:57:53 +0200

    commit for project presentation

--- js/code.js
+++ js/code.js
@@ -4,19 +4,20 @@ $('.button1').click(function(){
 	$('.button2').removeClass('active');
 	$('.button3').removeClass('active');
 	$('.button1').toggleClass('active');
-})
+});
 
 $('.button2').click(function(){
 	$('.button1').removeClass('active');
 	$('.button3').removeClass('active');
 	$('.button2').toggleClass('active');
-})
+});
 
 $('.button3').click(function(){
 	$('.button1').removeClass('active');
 	$('.button2').removeClass('active');
 	$('.button3').toggleClass('active');
-})
+});
+
 
 
 
